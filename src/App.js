287d955816bdc0f07fe7711021d61bd1fda4119c
Michael <kllmmc23@gmail.com>
2020-06-06 11:27:16 -0600

    understanding-children

--- src/App.js
+++ src/App.js
@@ -1,24 +1,15 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import Person from './Person';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <h1>hi Im a react app</h1>
+      <p>this is really working</p>
+      <Person name='Max' age='28'/>
+      <Person name='Manu' age='29'>My hobbies: Racing</Person>
+      <Person name='Stephanie' age='26'/>
+      
     </div>
   );
 }
