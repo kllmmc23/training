7720208879a4c13502e944f5e04ba8c5753d6fa2
Michael <kllmmc23@gmail.com>
2020-06-06 11:59:37 -0600

    manipulating state

--- src/App.js
+++ src/App.js
@@ -3,19 +3,24 @@ import Person from "./Person";
 import { Component } from "react";
 
 class App extends Component {
-
   state = {
-    persons: [
-    {name:'Max', age: 28}
-  ]
-}
+    persons: [{ name: "Max", age: 28 }],
+  };
+
+  switchNameHandler = () => {
+    console.log("worked");
+    // this.state.persons[0].name ='MAximilian' ///DOnt do this
+    this.setState({
+      persons: [{ name: "Mike" }],
+    });
+  };
 
   render() {
     return (
       <div className="App">
         <h1>hi Im a react app</h1>
         <p>this is really working</p>
-        <button onClick={}>Swtich Name</button>
+        <button onMouseDown={this.switchNameHandler}>Swtich Name</button>
         <Person name={this.state.persons[0].name} age="28" />
         <Person name="Manu" age="29">
           My hobbies: Racing
