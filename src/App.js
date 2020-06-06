586da1de87c39cc02ee71a49b17a1ae57e0fc2be
Michael <kllmmc23@gmail.com>
2020-06-06 13:32:09 -0600

    inline styles

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
-import Person from "./Person";
+import Person from "./Person/Person";
+import "./App.css";
 
 class App extends Component {
   state = {
@@ -20,15 +21,33 @@ class App extends Component {
       ],
     });
   };
+  namChangeHandler = (e) => {
+    this.setState({
+      person: [
+        { name: "Mike", age: "37" },
+        { name: e.target.value, age: "29" },
+        { name: "Stephanie", age: "27" },
+      ],
+    });
+  };
 
   render() {
+    const style = {
+      backgroundColor: "White",
+      font: 'inherit',
+      border: '1px solid blue',
+      padding: '8px',
+      cursor: 'pointer'
+      
+    };
+
     return (
       <div className="App">
         <h1>hi Im a react app</h1>
         <p>this is really working</p>
         {/* <button onClick={this.switchNameHandler.bind(this, "Mike")}> */}
-        <button onClick={() => this.switchNameHandler('Mike')}>
-          Swtich Name
+        <button  style={style} onClick={() => this.switchNameHandler("Mike")}>
+          Switch Name
         </button>
 
         <Person
@@ -39,6 +58,7 @@ class App extends Component {
           name={this.state.person[1].name}
           age={this.state.person[1].age}
           click={this.switchNameHandler.bind(this, "Michael")}
+          changed={this.namChangeHandler}
         >
           My hobbies: Racing
         </Person>
