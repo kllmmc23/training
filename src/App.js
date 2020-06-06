2121df50d6cd882720749e4e71977e3fe1053f72
Michael <kllmmc23@gmail.com>
2020-06-06 11:34:24 -0600

    understanding-using-state

--- src/App.js
+++ src/App.js
@@ -1,17 +1,29 @@
-import React from 'react';
-import Person from './Person';
+import React from "react";
+import Person from "./Person";
+import { Component } from "react";
 
-function App() {
-  return (
-    <div className='App'>
-      <h1>hi Im a react app</h1>
-      <p>this is really working</p>
-      <Person name='Max' age='28'/>
-      <Person name='Manu' age='29'>My hobbies: Racing</Person>
-      <Person name='Stephanie' age='26'/>
-      
-    </div>
-  );
+class App extends Component {
+
+  state = {
+    persons: [
+    {name:'Max', age: 28}
+  ]
+}
+
+  render() {
+    return (
+      <div className="App">
+        <h1>hi Im a react app</h1>
+        <p>this is really working</p>
+        <button>Swtich Name</button>
+        <Person name={this.state.persons[0].name} age="28" />
+        <Person name="Manu" age="29">
+          My hobbies: Racing
+        </Person>
+        <Person name="Stephanie" age="26" />
+      </div>
+    );
+  }
 }
 
 export default App;
